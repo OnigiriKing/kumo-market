e8d3c84ffbc3405bd317ef0a662908f1d18675ca
OnigiriKing <oooNikita87@gmail.com>
2023-08-16 10:26:14 +0700

    App

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./dist/style.css";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./pages/navBar/NavBar";
+import HomePage from "./pages/homePage/HomePage";
+import FooterPage from "./pages/footerPage/Footer";
+import DropMenu from "./pages/navBar/DropMenu";
+
+export default function App() {
+  
+
+  return (
+    <div className="App">
+      <NavBar />
+      <DropMenu />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      <FooterPage />
+    </div>
+  );
+}
