a74e1c012b6a4e37e5bea95068831539018528b8
OnigiriKing <oooNikita87@gmail.com>
2023-08-16 10:41:43 +0700

    navBar update

--- src/pages/navBar/NavBar.jsx
+++ src/pages/navBar/NavBar.jsx
@@ -6,6 +6,14 @@ export default function NavBar() {
   return (
     <nav id="nav-bar">
       <div className="nav-wrapper">
+      <div className="nav-logo">
+        Logo
+      </div>
+      <div className="nav-links">
+        <Link>Link</Link>
+        <Link>Link</Link>
+        <div className="nav-basket">Basket</div>
+      </div>
       </div>
     </nav>
   );
