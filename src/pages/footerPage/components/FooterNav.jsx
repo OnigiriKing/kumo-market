cf36bd11988573c2d97736b74cc6c47af77f0737
OnigiriKing <oooNikita87@gmail.com>
2023-08-16 15:36:22 +0700

    FooterNav base created

--- src/pages/footerPage/components/FooterNav.jsx
+++ src/pages/footerPage/components/FooterNav.jsx
@@ -1,8 +1,28 @@
 export default function FooterNav() {
 
    return (
-       <div className="footer-nav-screen">
-        Navigation
-       </div>
-    );
+     <div className="footer-nav-screen">
+       <ul>
+         <li>
+           <a href="#">About</a>
+         </li>
+         <li>
+           <a href="#">Locations</a>
+         </li>
+         <li>
+           <a href="#">FAQ</a>
+         </li>
+         <li>
+           <a href="#">News</a>
+         </li>
+         <li>
+           <a href="#">Careers</a>
+         </li>
+         <li>
+           <a href="#">Contact Us</a>
+         </li>
+       </ul>
+       <div>All rights reserver</div>
+     </div>
+   );
 }
