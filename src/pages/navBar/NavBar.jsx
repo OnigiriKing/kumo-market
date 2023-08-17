66aec9b8edac16fdc70c7e15c42164a545538d4c
OnigiriKing <oooNikita87@gmail.com>
2023-08-17 22:03:20 +0700

    Small fixes

--- src/pages/navBar/NavBar.jsx
+++ src/pages/navBar/NavBar.jsx
@@ -6,14 +6,14 @@ export default function NavBar() {
   return (
     <nav id="nav-bar">
       <div className="nav-wrapper">
-      <Link to="/" className="nav-logo">
-        Logo
-      </Link>
-      <div className="nav-links">
-        <Link>Link</Link>
-        <Link>Link</Link>
-        <div className="nav-basket">Basket</div>
-      </div>
+        <Link to="/" className="nav-logo">
+          Logo
+        </Link>
+        <div className="nav-links">
+          <Link>CATEGORIES</Link>
+          <Link>PRODUCT PAGE</Link>
+          <div className="nav-basket">Basket</div>
+        </div>
       </div>
     </nav>
   );
