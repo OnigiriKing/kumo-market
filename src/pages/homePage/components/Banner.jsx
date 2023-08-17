0426c184852b44c44046cdb578f284973ba01aab
OnigiriKing <oooNikita87@gmail.com>
2023-08-17 20:08:26 +0700

    Reverse banner style complete

--- src/pages/homePage/components/Banner.jsx
+++ src/pages/homePage/components/Banner.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import testImg from "../../../img/test2.png"
 
-export default function Banner(reverse = false) {
-  const content = reverse ? (
+export default function Banner({reverse = false}) {
+  const content = !reverse ? (
     <div id="banner-screen">
       <div className="banner-wrapper">
         <div className="banner-text">
@@ -22,7 +22,7 @@ export default function Banner(reverse = false) {
     <div id="banner-screen">
       <div className="banner-wrapper banner-reverse">
         <div className="banner-text">
-          <h2>Creative harmonious living</h2>
+          <h2>Cozy & Elegant Lifestyle</h2>
           <p>
             Our Products are all made to standard sizes so that you can mix and
             match them freely.
