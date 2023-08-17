84da0fc3ffb30868369743ed0b2b392626521bd1
OnigiriKing <oooNikita87@gmail.com>
2023-08-17 20:42:30 +0700

    Trending screen style complete

--- src/pages/homePage/components/Trending.jsx
+++ src/pages/homePage/components/Trending.jsx
@@ -1,16 +1,104 @@
+import { Link } from "react-router-dom";
+import testImg from "../../../img/test.png";
+
 export default function Trending() {
 
    return (
-     <div className="trending-screen">
+     <div id="trending-screen">
        <div className="trending-wrapper">
          <div className="trending-nav">
            <h2>Popilar Picks</h2>
            <div>
-             <button></button>
-             <button></button>
+             <button>Button</button>
+             <button>Button</button>
            </div>
          </div>
-         <div className="trending-items"></div>
+         <div className=" trending-items">
+           <div>
+             <Link to="/">
+               <img src={testImg} alt="product" />
+               <p>Name</p>
+               <h3>Price</h3>
+             </Link>
+           </div>
+           <div>
+             <Link to="/">
+               <img src={testImg} alt="product" />
+               <p>Name</p>
+               <h3>Price</h3>
+             </Link>
+           </div>
+           <div>
+             <Link to="/">
+               <img src={testImg} alt="product" />
+               <p>Name</p>
+               <h3>Price</h3>
+             </Link>
+           </div>
+           <div>
+             <Link to="/">
+               <img src={testImg} alt="product" />
+               <p>Name</p>
+               <h3>Price</h3>
+             </Link>
+           </div>
+           <div>
+             <Link to="/">
+               <img src={testImg} alt="product" />
+               <p>Name</p>
+               <h3>Price</h3>
+             </Link>
+           </div>
+           <div>
+             <Link to="/">
+               <img src={testImg} alt="product" />
+               <p>Name</p>
+               <h3>Price</h3>
+             </Link>
+           </div>
+           <div>
+             <Link to="/">
+               <img src={testImg} alt="product" />
+               <p>Name</p>
+               <h3>Price</h3>
+             </Link>
+           </div>
+           <div>
+             <Link to="/">
+               <img src={testImg} alt="product" />
+               <p>Name</p>
+               <h3>Price</h3>
+             </Link>
+           </div>
+           <div>
+             <Link to="/">
+               <img src={testImg} alt="product" />
+               <p>Name</p>
+               <h3>Price</h3>
+             </Link>
+           </div>
+           <div>
+             <Link to="/">
+               <img src={testImg} alt="product" />
+               <p>Name</p>
+               <h3>Price</h3>
+             </Link>
+           </div>
+           <div>
+             <Link to="/">
+               <img src={testImg} alt="product" />
+               <p>Name</p>
+               <h3>Price</h3>
+             </Link>
+           </div>
+           <div>
+             <Link to="/">
+               <img src={testImg} alt="product" />
+               <p>Name</p>
+               <h3>Price</h3>
+             </Link>
+           </div>
+         </div>
        </div>
      </div>
    );
