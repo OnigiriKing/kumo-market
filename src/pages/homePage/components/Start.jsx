50f0740f875cff194ec92be37f0875e947f04b84
OnigiriKing <oooNikita87@gmail.com>
2023-08-17 15:52:44 +0700

    Start screen base complete

--- src/pages/homePage/components/Start.jsx
+++ src/pages/homePage/components/Start.jsx
@@ -1,12 +1,46 @@
+import { Link } from "react-router-dom";
+import testImg from "../../../img/test.png"
+
 export default function Start() {
 
    return (
      <div id="start-screen">
        <div className="start-wrapper">
-         <div>Product 1</div>
-         <div>Product 2</div>
-         <div>Product 3</div>
-         <div>Product 4</div>
+         <div className="start-products">
+           <div>
+             <Link to="/">
+               <div className="start-img-cover"></div>
+               <img src={testImg}></img>
+               <p>Product</p>
+             </Link>
+           </div>
+
+           
+           <div>
+             <Link to="/">
+               <div className="start-img-cover"></div>
+               <img src={testImg}></img>
+               <p>Product</p>
+             </Link>
+           </div>
+           <div>
+             <Link to="/">
+               <div className="start-img-cover"></div>
+               <img src={testImg}></img>
+               <p>Product</p>
+             </Link>
+           </div>
+           <div>
+             <Link to="/">
+               <div className="start-img-cover"></div>
+               <img src={testImg}></img>
+               <p>Product</p>
+             </Link>
+           </div>
+
+
+
+         </div>
        </div>
      </div>
    );
