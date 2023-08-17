432e522c046e533df2bc21653392042328e18f6c
OnigiriKing <oooNikita87@gmail.com>
2023-08-17 19:58:14 +0700

    Banner screen style complete

--- src/pages/homePage/components/Banner.jsx
+++ src/pages/homePage/components/Banner.jsx
@@ -1,32 +1,37 @@
+import { Link } from "react-router-dom";
+import testImg from "../../../img/test2.png"
+
 export default function Banner(reverse = false) {
-  const content = !reverse ? (
-    <div className="banner-screen">
+  const content = reverse ? (
+    <div id="banner-screen">
       <div className="banner-wrapper">
-        <div>
+        <div className="banner-text">
           <h2>Creative harmonious living</h2>
           <p>
-            RAOUF Products are all made to standard sizes so that you can mix
-            and match them freely.
+            Our Products are all made to standard sizes so that you can mix and
+            match them freely.
           </p>
-          <button>Shop now</button>
+          <Link to={"/123"}>
+            <button>Shop now</button>
+          </Link>
         </div>
-        <div>Img</div>
+        <img src={testImg} className="banner-image"></img>
       </div>
     </div>
   ) : (
-    <div>
-      <div className="banner-screen">
-        <div className="banner-wrapper">
-          <div>
-            <h2>Creative harmonious living</h2>
-            <p>
-              RAOUF Products are all made to standard sizes so that you can mix
-              and match them freely.
-            </p>
+    <div id="banner-screen">
+      <div className="banner-wrapper banner-reverse">
+        <div className="banner-text">
+          <h2>Creative harmonious living</h2>
+          <p>
+            Our Products are all made to standard sizes so that you can mix and
+            match them freely.
+          </p>
+          <Link to={"/123"}>
             <button>Shop now</button>
-          </div>
-          <div>Img</div>
+          </Link>
         </div>
+        <img src={testImg} className="banner-image"></img>
       </div>
     </div>
   );
