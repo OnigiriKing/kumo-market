355f420b30716a11f8d25dd2af099d5eedf670a0
OnigiriKing <oooNikita87@gmail.com>
2023-08-19 23:41:44 +0700

    DisplayProducts update

--- src/scripts/displayProudcts.js
+++ src/scripts/displayProudcts.js
@@ -1,73 +1,84 @@
 import { Link } from "react-router-dom";
 import testImg from "../img/test.png";
 
-export function displayProducts(
-  caregory = "none",
-  newClass = "product-wrapper"
-) {
-  const productList = {
-    product1: {
-      name: "test",
-      price: 45,
-      img: testImg,
-      category: "skinCare",
-    },
-    product2: {
-      name: "test2",
-      price: 60,
-      img: testImg,
-      category: "skinCare",
-    },
-    product3: {
-      name: "test2",
-      price: 50,
-      img: testImg,
-      category: "skinCare",
-    },
-    product4: {
-      name: "test2",
-      price: 30,
-      img: testImg,
-      category: "Lamps",
-    },
-    product5: {
-      name: "test2",
-      price: 80,
-      img: testImg,
-      category: "Lamps",
-    },
-    product6: {
-      name: "test2",
-      price: 15,
-      img: testImg,
-      category: "Lamps",
-    },
-    product7: {
-      name: "test2",
-      price: 15,
-      img: testImg,
-      category: "Lamps",
-    },
-    product8: {
-      name: "test2",
-      price: 15,
-      img: testImg,
-      category: "Lamps",
-    },
-  };
+const productList = {
+  product1: {
+    name: "test",
+    price: 45,
+    img: testImg,
+    type: "skinCare",
+  },
+  product2: {
+    name: "test2",
+    price: 60,
+    img: testImg,
+    type: "skinCare",
+  },
+  product3: {
+    name: "test2",
+    price: 50,
+    img: testImg,
+    type: "skinCare",
+  },
+  product4: {
+    name: "test2",
+    price: 30,
+    img: testImg,
+    type: "Lamps",
+  },
+  product5: {
+    name: "test2",
+    price: 80,
+    img: testImg,
+    type: "lamps",
+  },
+  product6: {
+    name: "test2",
+    price: 15,
+    img: testImg,
+    type: "lamps",
+  },
+  product7: {
+    name: "test2",
+    price: 15,
+    img: testImg,
+    type: "lamps",
+  },
+  product8: {
+    name: "test2",
+    price: 15,
+    img: testImg,
+    type: "lamps",
+  },
+};
 
+export function displayProducts(
+  type = "all",
+  newClass = "product-wrapper",
+  number = "all"
+) {
   return (
     <div className={`${newClass}`}>
       {Object.keys(productList).map((key) => {
         const el = productList[key];
 
-        return (
-          <Link to="/" key={key} className="product">
-            <img src={el.img} alt="product" />
-            <p>{el.name}</p>
-            <h3>${el.price}</h3>
-          </Link>
-        );
+        if (type !== "all" && el.type === type) {
+          return (
+            <Link to="/" key={key} className="product">
+              <img src={el.img} alt="product" />
+              <p>{el.name}</p>
+              <h3>${el.price}</h3>
+            </Link>
+          );
+        } if (type === "all") {
+          return (
+            <Link to="/" key={key} className="product">
+              <img src={el.img} alt="product" />
+              <p>{el.name}</p>
+              <h3>${el.price}</h3>
+            </Link>
+          );
+        }
       })}
     </div>
   );
