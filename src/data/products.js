fc585a50e259a382ec0380675f82421df9cadbcf
OnigiriKing <oooNikita87@gmail.com>
2023-08-20 19:11:34 +0700

    Products updated

---
+++ src/data/products.js
@@ -0,0 +1,101 @@
+import acure from "./img/Acure.png"
+import astercook from "./img/Astercook.png";
+import caracole from "./img/Caracole.png";
+import carote from "./img/Carote.png";
+import ceraVe from "./img/CeraVe.png";
+import eknitey from "./img/Eknitey.png";
+import living from "./img/Living.png";
+import neutrogena from "./img/Neutrogena.png";
+import ordinary from "./img/Ordinary.png";
+import rae from "./img/Rae.png";
+import roda from "./img/Roda.png";
+import wlive from "./img/Wlive.png";
+
+const productList = {
+  product1: {
+    name: "Caracole Pretty Little Thing Chair",
+    price: 45,
+    img:  caracole ,
+    type: "chair",
+    des: "",
+  },
+  product2: {
+    name: "Roda Zania Chair",
+    price: 60,
+    img:  roda ,
+    type: "chair",
+    des: "",
+  },
+  product3: {
+    name: "Living and More High Back Sofa Chair",
+    price: 135,
+    img:  living ,
+    type: "chair",
+    des: "",
+  },
+  product4: {
+    name: "EKNITEY 2 Tier End Table",
+    price: 50,
+    img:  eknitey ,
+    type: "furniture",
+    des: "Perfect in any room! can be used as a side tables bedroom, coffee table in the living room, nightstand in the bedroom, casual table in the garden or patio, even an outdoor table or plant stand.",
+  },
+  product5: {
+    name: "WLIVE Dresser for Bedroom with 4 Drawers",
+    price: 89,
+    img:  wlive ,
+    type: "furniture",
+    des: "",
+  },
+  product6: {
+    name: "CAROTE 11pcs Pots and Pans Set",
+    price: 20,
+    img:  carote ,
+    type: "kitchen",
+    des: "",
+  },
+  product7: {
+    name: "Astercook Knife Set",
+    price: 49,
+    img:  astercook ,
+    type: "kitchen",
+    des: "",
+  },
+  product8: {
+    name: "Rae Dunn Kitchen Scissors- Stainless Steel Kitchen Shears",
+    price: 11,
+    img:  rae ,
+    type: "kitchen",
+    des: "",
+  },
+  product9: {
+    name: "Neutrogena Acne Proofing",
+    price: 75,
+    img:  neutrogena ,
+    type: "skin care",
+    des: "Acne-fighting formula is oil-free and from Neutrogena, the #1 dermatologist recommended acne brand",
+  },
+  product10: {
+    name: "THE ORDINARY Hyaluronic Acid 2% + B5 ",
+    price: 100,
+    img: ordinary,
+    type: "skin care",
+    des: "This formulation combines low-, medium- and high-molecular weight HA, as well as a next-generation HA crosspolymer at a combined concentration of 2% for multi-depth hydration in a water-based formula.",
+  },
+  product11: {
+    name: "Acure Brightening Facial Scrub",
+    price: 35,
+    img: acure,
+    type: "skin care",
+    des: "ACUREs Brightening collection incorporates plant extracts and antioxidants to improve skin's appearance while minimizing uneven skin tone, brightening and increasing radiance.",
+  },
+  product12: {
+    name: "CeraVe Facial Moisturizing Night Lotion",
+    price: 50,
+    img: ceraVe,
+    type: "skin care",
+    des: "Developed with dermatologists, CeraVe PM Facial Moisturizing Lotion has an ultra-lightweight, unique formula that moisturizes throughout the night and helps restore the protective skin barrier with three essential ceramides (1,3,6-II). ",
+  },
+};
+
+export default productList;
