810067792c1dd8ef890f74ef3f89ab1abcdad8fe
OnigiriKing <oooNikita87@gmail.com>
2023-08-20 11:34:14 +0700

    NavBar fix, logo created

--- src/pages/navBar/NavBar.jsx
+++ src/pages/navBar/NavBar.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import { allSvg } from "../../svg/allSvg";
 import { changeClass } from "../../scripts/changeClass";
+import logo from "../../img/logo.png"
 
 export default function NavBar() {
   return (
     <nav id="nav-bar">
-      <div className="nav-wrapper">
+      <div className="wrapper nav-wrapper">
         <Link to="/" className="nav-logo">
-          Logo
+          <img src={logo} />
         </Link>
         <div className="nav-links">
           <Link to="/categories/all">CATEGORIES</Link>
