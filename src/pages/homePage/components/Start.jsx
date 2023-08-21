eceeaf9607dd586bc71e164768f99dd504096766
OnigiriKing <oooNikita87@gmail.com>
2023-08-21 19:25:00 +0700

    Start update +img

--- src/pages/homePage/components/Start.jsx
+++ src/pages/homePage/components/Start.jsx
@@ -1,4 +1,8 @@
 import { Link } from "react-router-dom";
+import room from "../../../img/room.png"
+import kitchen from "../../../img/kitchen.png"
+import skinCare from "../../../img/skinCare.png"
+import chairs from "../../../img/chairs.png"
 
 export default function Start() {
 
@@ -7,32 +11,32 @@ export default function Start() {
        <div className="start-wrapper">
          <div className="start-products wrapper">
            <div>
-             <Link to="/">
+             <Link to="categories/all">
                <div className="start-img-cover"></div>
-               <img></img>
-               <p>Product</p>
+               <img src={room} />
+               <p>Live in comfort</p>
              </Link>
            </div>
 
            <div>
-             <Link to="/">
+             <Link to="categories/skin-care">
                <div className="start-img-cover"></div>
-               <img></img>
-               <p>Product</p>
+               <img src={skinCare} />
+               <p>Skin Care</p>
              </Link>
            </div>
            <div>
-             <Link to="/">
+             <Link to="categories/kitchen">
                <div className="start-img-cover"></div>
-               <img ></img>
-               <p>Product</p>
+               <img src={kitchen} />
+               <p>Kitchen</p>
              </Link>
            </div>
            <div>
-             <Link to="/">
+             <Link to="categories/chairs">
                <div className="start-img-cover"></div>
-               <img ></img>
-               <p>Product</p>
+               <img src={chairs} />
+               <p>Chairs</p>
              </Link>
            </div>
          </div>
