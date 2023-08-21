28b2bc7d67a52a1b85d1c718b7cc0a90c83a1086
OnigiriKing <oooNikita87@gmail.com>
2023-08-21 15:32:05 +0700

    Products are ready, description added

--- src/data/products.js
+++ src/data/products.js
@@ -1,100 +1,101 @@
-import acure from "./img/Acure.png";
-import astercook from "./img/Astercook.png";
-import caracole from "./img/Caracole.png";
-import carote from "./img/Carote.png";
-import ceraVe from "./img/CeraVe.png";
-import eknitey from "./img/Eknitey.png";
-import living from "./img/Living.png";
-import neutrogena from "./img/Neutrogena.png";
-import ordinary from "./img/Ordinary.png";
-import rae from "./img/Rae.png";
-import roda from "./img/Roda.png";
-import wlive from "./img/Wlive.png";
+import chairs1 from "./img/chairs1.png"
+import chairs2 from "./img/chairs2.png"
+import chairs3 from "./img/chairs3.png"
+import furniture1 from "./img/furniture1.png"
+import furniture2 from "./img/furniture2.png"
+import kitchen1 from "./img/kitchen1.png"
+import kitchen2 from "./img/kitchen2.png"
+import kitchen3 from "./img/kitchen3.png"
+import skinCare1 from "./img/skinCare1.png"
+import skinCare2 from "./img/skinCare2.png"
+import skinCare3 from "./img/skinCare3.png"
+import skinCare4 from "./img/skinCare4.png"
+
 
 const productList = {
   product1: {
     name: "Nature Series Cream Beige DSW",
     price: 68,
-    img: caracole,
+    img: chairs1,
     type: "chairs",
     des: "Each color in the Nature Series resembles the natural color created by our mother Earth, hence these colors are organically and naturally looking. These natural tones of colors are meant to blend in all kind of settings and occasions. ",
   },
   product2: {
     name: "Strandmon High Back Chair",
     price: 120,
-    img: roda,
+    img: chairs2,
     type: "chairs",
     des: "You can really relax and unwind in comfort as this sofa's high back provides extra neck support. The cover is made of soft velvet fabric with wide stripes, pleasant to the touch, has a slight sheen and makes the chair cozy.",
   },
   product3: {
     name: "Halmar Armchairs SOFT",
     price: 135,
-    img: living,
+    img: chairs3,
     type: "chairs",
     des: "All furniture offered by Halmar is durable, durable, as well as functional and comfortable in everyday use. They combine modern, original design with many utility values. By choosing the Halmar brand, you can be sure that furniture and accessories will be an excellent element of interior design and will allow you to create many unique arrangements.",
   },
   product4: {
     name: "Polytrends Laguna 47'",
     price: 800,
-    img: eknitey,
+    img: furniture1,
     type: "furniture",
     des: "Designed with clean, straight lines and topped with a unique headboard, the Farmhouse Collection offers unparalleled class and waterfront style. Each piece is constructed of lumber made of durable recycled high-density polyethylene (HDPE) that provides the look of painted wood without the maintenance. ",
   },
   product5: {
     name: "FÖRHÖJA Kitchen cart",
     price: 200,
-    img: wlive,
+    img: furniture2,
     type: "furniture",
     des: "Solid wood is a natural material that is easy to repair, refinish and change whenever you like. Birch is a straight-grained and light-coloured hardwood with light brown knots. Over time the colour goes from cream to light amber.",
   },
   product6: {
     name: "Philips HR 3652/00",
     price: 20,
-    img: carote,
+    img: kitchen1,
     type: "kitchen",
     des: "With ProBlend 6 3D technology, 1400 watts of power and speeds up to 35,000 RPM, enjoy smoother smoothies and blends with just the right taste and texture. Proven to motivate users to increase fruit and vegetables in their diet.",
   },
   product7: {
     name: "KitchenAid Artisan KSM15",
     price: 59,
-    img: astercook,
+    img: kitchen2,
     type: "kitchen",
     des: "Elevate your culinary creations with the KitchenAid Artisan KSM150 Stand Mixer. This iconic kitchen essential combines style and power. With its 325-watt motor and versatile attachments, it effortlessly handles everything from mixing dough to whipping up delicate meringues. ",
   },
   product8: {
-    name: "Bialetti Moka Express Espresso Maker",
+    name: "Bialetti Moka Express",
     price: 70,
-    img: rae,
+    img: kitchen3,
     type: "kitchen",
     des: "Experience the rich tradition of Italian coffee-making with the Bialetti Moka Express. This iconic stovetop espresso maker has been a beloved fixture in kitchens worldwide for generations.",
   },
   product9: {
     name: "The Ordinary Glycolic Acid 7%",
     price: 23,
-    img: neutrogena,
+    img: skinCare1,
     type: "skin care",
     des: "Unlock radiant skin with The Ordinary Glycolic Acid 7%. This skincare powerhouse is formulated to exfoliate and revitalize your complexion, leaving it smoother, brighter, and more even-toned.",
   },
   product10: {
     name: "iS Clinical Super Serum Advance+",
     price: 68,
-    img: ordinary,
+    img: skinCare2,
     type: "skin care",
     des: "Elevate your skincare routine with iS Clinical Super Serum Advance+. This advanced formula is a powerful ally in your quest for healthier, more youthful skin.",
   },
   product11: {
     name: "La Roche-Posay Lipikar Fluide",
     price: 45,
-    img: acure,
+    img: skinCare3,
     type: "skin care",
     des: "Discover La Roche-Posay Lipikar Fluide, a gentle and hydrating body lotion designed to nourish and soothe your skin. Formulated with the utmost care and backed by dermatological expertise, this lightweight, non-greasy fluid is perfect for sensitive or dry skin.",
   },
   product12: {
-    name: "CHANEL LA MOUSSE Anti-Pollution",
+    name: "Chanel La Mousse",
     price: 77,
-    img: ceraVe,
+    img: skinCare4,
     type: "skin care",
-    des: "Elevate your skincare experience with CHANEL LA MOUSSE Anti-Pollution. This luxurious cleansing mousse is your daily defense against environmental pollutants and impurities. Crafted by CHANEL, a name synonymous with timeless elegance and quality, this mousse transforms your cleansing routine into a spa-like indulgence.",
+    des: "Elevate your skincare experience with Chanel La Mousse Anti-Pollution. This luxurious cleansing mousse is your daily defense against environmental pollutants and impurities. Crafted by CHANEL, a name synonymous with timeless elegance and quality, this mousse transforms your cleansing routine into a spa-like indulgence.",
   },
 };
 
