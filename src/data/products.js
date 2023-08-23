595c1060196a9555c92dd324213fe9cb93789801
OnigiriKing <oooNikita87@gmail.com>
2023-08-23 16:59:39 +0700

    products update

--- src/data/products.js
+++ src/data/products.js
@@ -18,6 +18,8 @@ const productList = {
     price: 68,
     img: chairs1,
     type: "chairs",
+    link: "product/1",
+    texture: "wood",
     des: "Each color in the Nature Series resembles the natural color created by our mother Earth, hence these colors are organically and naturally looking. These natural tones of colors are meant to blend in all kind of settings and occasions. ",
   },
   product2: {
@@ -25,6 +27,8 @@ const productList = {
     price: 120,
     img: chairs2,
     type: "chairs",
+    link: "product/2",
+    texture: "wood",
     des: "You can really relax and unwind in comfort as this sofa's high back provides extra neck support. The cover is made of soft velvet fabric with wide stripes, pleasant to the touch, has a slight sheen and makes the chair cozy.",
   },
   product3: {
@@ -32,6 +36,8 @@ const productList = {
     price: 135,
     img: chairs3,
     type: "chairs",
+    link: "product/3",
+    texture: "wood",
     des: "All furniture offered by Halmar is durable, durable, as well as functional and comfortable in everyday use. They combine modern, original design with many utility values. By choosing the Halmar brand, you can be sure that furniture and accessories will be an excellent element of interior design and will allow you to create many unique arrangements.",
   },
   product4: {
@@ -39,6 +45,8 @@ const productList = {
     price: 800,
     img: furniture1,
     type: "furniture",
+    link: "product/4",
+    texture: "polyethylene",
     des: "Designed with clean, straight lines and topped with a unique headboard, the Farmhouse Collection offers unparalleled class and waterfront style. Each piece is constructed of lumber made of durable recycled high-density polyethylene (HDPE) that provides the look of painted wood without the maintenance. ",
   },
   product5: {
@@ -46,6 +54,8 @@ const productList = {
     price: 200,
     img: furniture2,
     type: "furniture",
+    link: "product/5",
+    texture: "wood",
     des: "Solid wood is a natural material that is easy to repair, refinish and change whenever you like. Birch is a straight-grained and light-coloured hardwood with light brown knots. Over time the colour goes from cream to light amber.",
   },
   product6: {
@@ -53,6 +63,8 @@ const productList = {
     price: 20,
     img: kitchen1,
     type: "kitchen",
+    link: "product/6",
+    texture: "metal",
     des: "With ProBlend 6 3D technology, 1400 watts of power and speeds up to 35,000 RPM, enjoy smoother smoothies and blends with just the right taste and texture. Proven to motivate users to increase fruit and vegetables in their diet.",
   },
   product7: {
@@ -60,6 +72,8 @@ const productList = {
     price: 59,
     img: kitchen2,
     type: "kitchen",
+    link: "product/7",
+    texture: "metal",
     des: "Elevate your culinary creations with the KitchenAid Artisan KSM150 Stand Mixer. This iconic kitchen essential combines style and power. With its 325-watt motor and versatile attachments, it effortlessly handles everything from mixing dough to whipping up delicate meringues. ",
   },
   product8: {
@@ -67,6 +81,8 @@ const productList = {
     price: 70,
     img: kitchen3,
     type: "kitchen",
+    link: "product/8",
+    texture: "metal",
     des: "Experience the rich tradition of Italian coffee-making with the Bialetti Moka Express. This iconic stovetop espresso maker has been a beloved fixture in kitchens worldwide for generations.",
   },
   product9: {
@@ -74,6 +90,8 @@ const productList = {
     price: 23,
     img: skinCare1,
     type: "skin care",
+    link: "product/9",
+    texture: "liquid",
     des: "Unlock radiant skin with The Ordinary Glycolic Acid 7%. This skincare powerhouse is formulated to exfoliate and revitalize your complexion, leaving it smoother, brighter, and more even-toned.",
   },
   product10: {
@@ -81,6 +99,8 @@ const productList = {
     price: 68,
     img: skinCare2,
     type: "skin care",
+    link: "product/10",
+    texture: "liquid",
     des: "Elevate your skincare routine with iS Clinical Super Serum Advance+. This advanced formula is a powerful ally in your quest for healthier, more youthful skin.",
   },
   product11: {
@@ -88,6 +108,8 @@ const productList = {
     price: 45,
     img: skinCare3,
     type: "skin care",
+    link: "product/11",
+    texture: "liquid",
     des: "Discover La Roche-Posay Lipikar Fluide, a gentle and hydrating body lotion designed to nourish and soothe your skin. Formulated with the utmost care and backed by dermatological expertise, this lightweight, non-greasy fluid is perfect for sensitive or dry skin.",
   },
   product12: {
@@ -95,6 +117,8 @@ const productList = {
     price: 77,
     img: skinCare4,
     type: "skin care",
+    link: "product/12",
+    texture: "creame",
     des: "Elevate your skincare experience with Chanel La Mousse Anti-Pollution. This luxurious cleansing mousse is your daily defense against environmental pollutants and impurities. Crafted by CHANEL, a name synonymous with timeless elegance and quality, this mousse transforms your cleansing routine into a spa-like indulgence.",
   },
 };
