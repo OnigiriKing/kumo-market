ce002e8bd6781f19240783467569ae7e8873bae3
OnigiriKing <oooNikita87@gmail.com>
2023-08-25 16:47:13 +0700

    NavBar update

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -7,6 +7,7 @@ import React from "react";
 
 
 export default function HomePage({ cat, setCat }) {
+
   return (
     <div id="home-page">
       <Start cat={cat} setCat={setCat} />
--- src/pages/navBar/NavBar.jsx
+++ src/pages/navBar/NavBar.jsx
@@ -4,6 +4,8 @@ import { changeClass } from "../../scripts/changeClass";
 import logo from "../../img/logo.png"
 
 export default function NavBar() {
+
+  const randomProduct = Math.floor(Math.random() * 12) + 1;
   return (
     <nav id="nav-bar">
       <div className="wrapper nav-wrapper">
@@ -12,7 +14,7 @@ export default function NavBar() {
         </Link>
         <div className="nav-links">
           <Link to="/categories/all">CATEGORIES</Link>
-          <Link to="/product">PRODUCT PAGE</Link>
+          <Link to={`/product/${randomProduct}`}>PRODUCT PAGE</Link>
           <div className="nav-basket">{allSvg(30).basket}</div>
         </div>
       </div>
