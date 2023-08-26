21744e666f243e16667a56c53c3d78913b533249
OnigiriKing <oooNikita87@gmail.com>
2023-08-26 13:47:09 +0700

    Overlay is created

---
+++ src/pages/overlay/Overlay.jsx
@@ -0,0 +1,13 @@
+import CartMenu from "./components/DropMenu";
+import DropMenu from "./components/CartMenu";
+
+
+export default function Overlay() {
+
+    return (
+        <>
+        <DropMenu />
+        <CartMenu />
+        </>
+    )
+}
