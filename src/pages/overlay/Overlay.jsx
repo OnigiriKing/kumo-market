f2ecd9a0c2ff3048cfd73533d7548592fc099dc7
OnigiriKing <oooNikita87@gmail.com>
2023-08-27 16:32:47 +0700

    Amount valut change implemented

--- src/pages/overlay/Overlay.jsx
+++ src/pages/overlay/Overlay.jsx
@@ -2,11 +2,15 @@ import CartMenu from "./components/CartMenu";
 import DropMenu from "./components/DropMenu";
 
 
-export default function Overlay({ cartItems, setCartItems }) {
+export default function Overlay({ cartItems, setCartItems, cartCount }) {
   return (
     <>
       <DropMenu />
-      <CartMenu cartItems={cartItems} setCartItems={setCartItems}/>
+      <CartMenu
+        cartItems={cartItems}
+        setCartItems={setCartItems}
+        cartCount={cartCount}
+      />
     </>
   );
 }
--- src/pages/overlay/components/CartMenu.jsx
+++ src/pages/overlay/components/CartMenu.jsx
@@ -7,73 +7,82 @@ import React from "react";
 
 
 
-export default function CartMenu({ cartItems, setCartItems }) {
-
+export default function CartMenu({ cartItems, setCartItems, cartCount }) {
   const [subtotal, setSubtotal] = React.useState(0);
 
+  // updates subtotal
   React.useEffect(() => {
     let result = 0;
-    Object.keys(cartItems).map((key)=> {
+    Object.keys(cartItems).map((key) => {
       const el = cartItems[key];
-      return result += el.price * el.amount
-    })
+      return (result += el.price * el.amount);
+    });
     setSubtotal(result);
   }, [cartItems]);
 
+  // function to delete product
+  function deleteObject(key) {
+    const updatedCartItems = { ...cartItems };
+    delete updatedCartItems[key];
+    setCartItems(updatedCartItems);
+  }
 
-  const allProducts = (
-    Object.keys(cartItems).map((key)=> {
-
-      function deleteObject(key) {
-        const updatedCartItems = { ...cartItems };
-        delete updatedCartItems[key];
-        setCartItems(updatedCartItems);
+  // To change amount of product
+  function changeAmount(type, key) {
+    const baseObject = { ...cartItems };
+    if (type == "plus") {
+      if (baseObject[key].amount < 10) {
+        baseObject[key].amount++;
       }
-      const el = cartItems[key];
-      return (
-        <div className="cart-product" key={key}>
-          <div className="cart-product-img">
-            <img src={el.img} />
+    }
+    if (type == "minus") {
+      if (baseObject[key].amount > 1) {
+        baseObject[key].amount--;
+      }
+    }
+    setCartItems(baseObject);
+  }
+
+  // creates a product from state
+  const allProducts = Object.keys(cartItems).map((key) => {
+    const el = cartItems[key];
+    return (
+      <div className="cart-product" key={key}>
+        <div className="cart-product-img">
+          <img src={el.img} />
+        </div>
+        <div className="cart-product-des">
+          <div className="cart-product-price">
+            <p>{el.name}</p>
+            <h3>{el.price * el.amount}$</h3>
           </div>
-          <div className="cart-product-des">
-            <div className="cart-product-price">
-              <p>{el.name}</p>
-              <h3>{el.price * el.amount}$</h3>
+          <div className="cart-product-quantity">
+            <div className="cart-product-btns">
+              <button onClick={() => changeAmount("minus", key)}>-</button>
+              <h2>{el.amount}</h2>
+              <button onClick={() => changeAmount("plus", key)}>+</button>
             </div>
-            <div className="cart-product-quantity">
-              <div className="cart-product-btns">
-                <button>-</button>
-                <h2>{el.amount}</h2>
-                <button>+</button>
-              </div>
-              <div
-                className="cart-delete-btn"
-                onClick={() => deleteObject(key)}
-              >
-                {allSvg(25).closeBtn}
-              </div>
+            <div className="cart-delete-btn" onClick={() => deleteObject(key)}>
+              {allSvg(25).closeBtn}
             </div>
           </div>
         </div>
-      );
-    })
-
-  );
+      </div>
+    );
+  });
 
   return (
     <>
       <div className="full-overlay" {...closeCart()} />
       <div className="cart-menu">
         <div className="cart-menu-des">
-          <h3>Your Shopping Cart (8)</h3>
+          <h3>Your Shopping Cart ({cartCount})</h3>
           <div className="cart-close-button" {...closeCart()}>
             {allSvg(30).closeBtn}
           </div>
         </div>
         <div className="cart-menu-container">
-          <div className="cart-menu-products">
-          {allProducts}
-          </div>
+          <div className="cart-menu-products">{allProducts}</div>
           <div className="cart-menu-checkout">
             <div>
               <h3>Subtotal</h3>
