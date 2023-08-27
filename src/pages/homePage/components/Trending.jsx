7de9ab78adae5604f892ae6811cd0d4467905ae7
OnigiriKing <oooNikita87@gmail.com>
2023-08-27 09:35:06 +0700

    Scroll btns added

--- src/pages/homePage/components/Trending.jsx
+++ src/pages/homePage/components/Trending.jsx
@@ -4,14 +4,22 @@ import { allSvg } from "../../../svg/allSvg";
 
 export default function Trending() {
 
+  function scroll(direction) {
+    if (direction == "right") {
+      document.querySelector(".trending-items").scrollBy(225, 0);
+    } else {
+      document.querySelector(".trending-items").scrollBy(-225, 0);
+    }
+  }
+
    return (
      <div id="trending-screen">
        <div className="wrapper trending-wrapper">
          <div className="trending-nav">
            <h2>Popilar Picks</h2>
            <div className="trending-btns">
-             <button>{allSvg(20).arrowSides}</button>
-             <button>{allSvg(20).arrowSides}</button>
+             <button onClick={() => scroll("left")}>{allSvg(20).arrowSides}</button>
+             <button onClick={() => scroll("right")}>{allSvg(20).arrowSides}</button>
            </div>
          </div>
          <DisplayProducts newClass="trending-items" />
