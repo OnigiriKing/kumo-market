8046b755e465e4a1ff828e48d3b1648a8ac1e126
OnigiriKing <oooNikita87@gmail.com>
2023-08-27 18:08:02 +0700

    Empty cart settings added

--- src/pages/overlay/components/CartMenu.jsx
+++ src/pages/overlay/components/CartMenu.jsx
@@ -1,11 +1,7 @@
-
 import { allSvg } from "../../../svg/allSvg";
-import products from "../../../data/products"
 import { closeCart } from "../../../scripts/changeClass";
 import React from "react";
-
-
-
+import emptyCart from "../../../img/emptyCart.png"
 
 export default function CartMenu({ cartItems, setCartItems, cartCount }) {
   const [subtotal, setSubtotal] = React.useState(0);
@@ -51,7 +47,7 @@ export default function CartMenu({ cartItems, setCartItems, cartCount }) {
         <div className="cart-product-img">
           <img src={el.img} />
         </div>
-        <div className="cart-product-des">
+        <div className=" cart-product-des">
           <div className="cart-product-price">
             <p>{el.name}</p>
             <h3>{el.price * el.amount}$</h3>
@@ -81,16 +77,30 @@ export default function CartMenu({ cartItems, setCartItems, cartCount }) {
             {allSvg(30).closeBtn}
           </div>
         </div>
-        <div className="cart-menu-container">
-          <div className="cart-menu-products">{allProducts}</div>
-          <div className="cart-menu-checkout">
-            <div>
-              <h3>Subtotal</h3>
-              <h3>{subtotal}$</h3>
-            </div>
-            <button>Checkout</button>
+        {cartCount < 1 && (
+          <div className="cart-no-items">
+            <img src={emptyCart}></img>
+            <h2>Your cart is empty</h2>
+            <button className="no-items-btn" {...closeCart()}>
+              Keep Browsing
+            </button>
           </div>
-        </div>
+        )}
+        {cartCount >= 1 && (
+          <div className="cart-menu-container">
+            <div className="cart-menu-products">
+              {cartCount < 1 && <div>You have no items</div>}
+              {allProducts}
+            </div>
+            <div className="cart-menu-checkout">
+              <div>
+                <h3>Subtotal</h3>
+                <h3>{subtotal}$</h3>
+              </div>
+              <button className="checkout-btn">Checkout</button>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
