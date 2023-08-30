e7053cb81f95723649707e0108865ae924474946
OnigiriKing <oooNikita87@gmail.com>
2023-08-30 16:37:01 +0700

    Product.jsx fix

--- src/pages/productPage/Product.jsx
+++ src/pages/productPage/Product.jsx
@@ -1,7 +1,7 @@
 import Trending from "../homePage/components/Trending";
 import React from "react";
 import { useParams } from "react-router-dom";
-import ProductInfo from "./components/ProductInfo.jsx";
+import ProductInfo from "/Users/nikitawr/Dev/kumo-market/src/pages/productPage/components/ProductInfo.jsx";
 
 export default function Product({ setCartItems, cartItems }) {
   const { id } = useParams();
