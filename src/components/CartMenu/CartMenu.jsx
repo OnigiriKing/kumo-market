96a3ef0aacec4ec7ca2ff90d72b6a12d4ec1bbf4
OnigiriKing <oooNikita87@gmail.com>
2023-11-26 23:19:59 +0700

    Localization update

--- src/components/CartMenu/CartMenu.jsx
+++ src/components/CartMenu/CartMenu.jsx
@@ -4,10 +4,13 @@ import React from "react";
 import emptyCart from "img/navBar/emptyCart.png";
 import deleteObj from "./CartMenu.deleteObject";
 import changeAm from "./CartMenu.changeAmount";
+import { useTranslation } from "react-i18next";
 
 
 export default function CartMenu({ cartItems, setCartItems, cartCount }) {
   const [subtotal, setSubtotal] = React.useState(0);
+
+    const { t } = useTranslation();
 
   // updates subtotal
   React.useEffect(() => {
@@ -22,6 +25,7 @@ export default function CartMenu({ cartItems, setCartItems, cartCount }) {
   // creates a product from state
   const allProducts = Object.keys(cartItems).map((key) => {
     const el = cartItems[key];
+
     return (
       <div className="cart-product" key={key}>
         <div className="cart-product-img">
@@ -67,7 +71,9 @@ export default function CartMenu({ cartItems, setCartItems, cartCount }) {
       <div className="full-overlay" onClick={() => handleCartClick()} />
       <div className="cart-menu">
         <div className="cart-menu-des">
-          <h3>Your Shopping Cart ({cartCount})</h3>
+          <h3>
+            {t("YOURCART")}({cartCount})
+          </h3>
           <div className="cart-close-button" onClick={() => handleCartClick()}>
             {allSvg(30).closeBtn}
           </div>
@@ -75,24 +81,24 @@ export default function CartMenu({ cartItems, setCartItems, cartCount }) {
         {cartCount < 1 && (
           <div className="cart-no-items">
             <img src={emptyCart} alt="empty"></img>
-            <h2>Your cart is empty</h2>
+            <h2>{t("CARTEMPTY")}</h2>
             <button className="no-items-btn" onClick={() => handleCartClick()}>
-              Keep Browsing
+              {t("BROWSING")}
             </button>
           </div>
         )}
         {cartCount >= 1 && (
           <div className="cart-menu-container">
             <div className="cart-menu-products">
-              {cartCount < 1 && <div>You have no items</div>}
+              {cartCount < 1 && <div>{t("NOITEMS")}</div>}
               {allProducts}
             </div>
             <div className="cart-menu-checkout">
               <div>
-                <h3>Subtotal</h3>
+                <h3>{t("SUBTOTAL")}</h3>
                 <h3>{subtotal}$</h3>
               </div>
-              <button className="checkout-btn">Checkout</button>
+              <button className="checkout-btn">{t("CHECKOUT")}</button>
             </div>
           </div>
         )}
