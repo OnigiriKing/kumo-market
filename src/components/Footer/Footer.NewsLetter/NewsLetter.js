c9d21ce928049174c5b6548c7f37370025402f2b
OnigiriKing <oooNikita87@gmail.com>
2023-11-26 23:28:46 +0700

    Netlify bug fix 2

--- src/components/Footer/Footer.NewsLetter/NewsLetter.js
+++ src/components/Footer/Footer.NewsLetter/NewsLetter.js
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 
 export default function NewsLetter() {
 
-      const { t, i18n } = useTranslation();
+      const { t } = useTranslation();
 
    return (
      <div className="newsletter-screen">
