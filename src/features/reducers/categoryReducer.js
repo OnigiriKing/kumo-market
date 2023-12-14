06bbb72cbdd4856ab317a5dbb6c6145f555ff938
OnigiriKing <oooNikita87@gmail.com>
2023-12-14 21:05:37 +0700

    _redirects added

--- src/features/reducers/categoryReducer.js
+++ src/features/reducers/categoryReducer.js
@@ -2,10 +2,15 @@ import * as actions from "../actionTypes";
 
 const initialState = "all";
 
+// const initialState = localStorage.getItem("categoryName")
+//   ? parseInt(localStorage.getItem("categoryName"), 10)
+//   : "all";
+
 export default function categoryReducer(state = initialState, action) {
   console.log(action);
   switch (action.type) {
     case actions.SET_CATEGORY: {
+      localStorage.setItem("categoryName", action.payload.toString());
       const { category } = action.payload;
       return category;
     }
