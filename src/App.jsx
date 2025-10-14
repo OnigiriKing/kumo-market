147d1eb331798af75ad16d52526b9933346b851c
OnigiriKing <oooNikita87@gmail.com>
2025-10-14 10:33:00 +0900

    lazy added

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,56 @@
-import React from "react";
+import { useEffect, lazy, Suspense } from "react";
 import "./dist/style.css";
 import { Route, Routes } from "react-router-dom";
-import NavPage from "pages/navPage/navPage";
 import HomePage from "pages/homePage/HomePage";
-import FooterPage from "pages/footerPage/FooterPage";
-import CategoriesPage from "pages/categoriesPage/CategoriesPage";
 import ProductPage from "pages/productPage/ProductPage";
+import NavPage from "pages/navPage/navPage";
+import FooterPage from "pages/footerPage/FooterPage";
 import Overlay from "pages/overlay/Overlay";
 import { useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setCount } from "features/reducers/cartCountSlice";
 
 export default function App() {
-
   // state
   const dispatch = useDispatch();
   const cartItems = useSelector((store) => store.cartItems);
 
+  useEffect(() => {
+    try {
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    } catch (e) {
+      console.error("Failed to save state to local storage:", e);
+    }
+  }, [cartItems]);
 
-    React.useEffect(() => {
-      try {
-        localStorage.setItem("cartItems", JSON.stringify(cartItems));
-      } catch (e) {
-        console.error("Failed to save state to local storage:", e);
-      }
-    }, [cartItems]);
-
-  React.useEffect(() => {
-    dispatch(setCount({items: cartItems}));
+  useEffect(() => {
+    dispatch(setCount({ items: cartItems }));
   }, [cartItems, dispatch]);
 
   const html = useLocation().pathname;
 
-  React.useEffect(() => {
+  useEffect(() => {
     window.scrollTo({ top: 0 });
   }, [html]);
+
+  // React lazy
+
+  const CategoriesPage = lazy(() =>
+    import("pages/categoriesPage/CategoriesPage")
+  );
 
 
   return (
     <div className="app">
       <Overlay />
       <NavPage />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/categories/*" element={<CategoriesPage />} />
-        <Route path="/product/:id" element={<ProductPage />} />
-      </Routes>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/categories/*" element={<CategoriesPage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+        </Routes>
+      </Suspense>
       <FooterPage />
     </div>
   );
